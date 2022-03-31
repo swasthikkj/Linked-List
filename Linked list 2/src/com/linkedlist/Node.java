128c44da126f0c749de3f07ff084dc41660e465c
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-03-31 19:40:17 +0530

    program

---
+++ Linked list 2/src/com/linkedlist/Node.java
@@ -0,0 +1,34 @@
+package com.linkedlist;
+
+public class Node {
+	int data;
+	Node next;
+
+	public Node(int data) {
+		
+	}
+		public Node() {
+			super();
+			this.data = data;
+		}
+		
+		public int getData() {
+			return data;
+		}
+		
+		public void setData(int data) {
+			this.data = data;
+		}
+		
+		public Node getNext() {
+			return next;
+		}
+		
+		public void setNext(Node next) {
+			this.next = next;
+		}
+
+		public void display() {
+			System.out.println(" "+data);
+	}
+}
